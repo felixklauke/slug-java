1e82a01be3922927ede5be5128c4258ee44c6fe9
JackWhite20 <phil.westf@web.de>
2018-02-11 23:41:46 +0100

    Add complete lexer logic with all token types and finish the test for the lexer

--- src/test/java/net/jackwhite20/slug/lexer/LexerTest.java
+++ src/test/java/net/jackwhite20/slug/lexer/LexerTest.java
@@ -25,46 +25,313 @@ import org.junit.Test;
 public class LexerTest {
 
     @Test
-    public void nextToken() throws Exception {
-        Lexer lexer = new Lexer(", + // test //");
+    public void testClass() {
+        Lexer lexer = new Lexer("class");
 
-        Token comma = lexer.nextToken();
-        Assert.assertEquals(TokenType.COMMA, comma.getTokenType());
-        Assert.assertEquals(",", comma.getValue());
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.CLASS, token.getTokenType());
+        Assert.assertEquals("class", token.getValue());
+    }
 
-        Token plus = lexer.nextToken();
-        Assert.assertEquals(TokenType.PLUS, plus.getTokenType());
-        Assert.assertEquals("+", plus.getValue());
+    @Test
+    public void testFunc() {
+        Lexer lexer = new Lexer("func");
 
-        Token none = lexer.nextToken();
-        Assert.assertEquals(TokenType.NONE, none.getTokenType());
-        Assert.assertEquals("NONE", none.getValue());
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.FUNC, token.getTokenType());
+        Assert.assertEquals("func", token.getValue());
+    }
+
+    @Test
+    public void testCurlyLeft() {
+        Lexer lexer = new Lexer("{");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.CURLY_LEFT_PARAN, token.getTokenType());
+        Assert.assertEquals("{", token.getValue());
+    }
+
+    @Test
+    public void testCurlyRight() {
+        Lexer lexer = new Lexer("}");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.CURLY_RIGHT_PARAN, token.getTokenType());
+        Assert.assertEquals("}", token.getValue());
+    }
+
+    @Test
+    public void testLeftParan() {
+        Lexer lexer = new Lexer("(");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.LEFT_PARAN, token.getTokenType());
+        Assert.assertEquals("(", token.getValue());
+    }
+
+    @Test
+    public void testRightParan() {
+        Lexer lexer = new Lexer(")");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.RIGHT_PARAN, token.getTokenType());
+        Assert.assertEquals(")", token.getValue());
+    }
+
+    @Test
+    public void testSemicolon() {
+        Lexer lexer = new Lexer(";");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.SEMICOLON, token.getTokenType());
+        Assert.assertEquals(";", token.getValue());
+    }
+
+    @Test
+    public void testComma() {
+        Lexer lexer = new Lexer(",");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.COMMA, token.getTokenType());
+        Assert.assertEquals(",", token.getValue());
+    }
+
+    @Test
+    public void testName() {
+        Lexer lexer = new Lexer("Test");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.NAME, token.getTokenType());
+        Assert.assertEquals("Test", token.getValue());
+    }
+
+    @Test
+    public void testCall() {
+        Lexer lexer = new Lexer("WriteLine()");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.CALL, token.getTokenType());
+        Assert.assertEquals("WriteLine", token.getValue());
+    }
+
+    @Test
+    public void testReturn() {
+        Lexer lexer = new Lexer("return");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.RETURN, token.getTokenType());
+        Assert.assertEquals("return", token.getValue());
+    }
+
+    @Test
+    public void testBoolType() {
+        Lexer lexer = new Lexer("bool");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.BOOL, token.getTokenType());
+        Assert.assertEquals("bool", token.getValue());
+    }
+
+    @Test
+    public void testBoolValueFalse() {
+        Lexer lexer = new Lexer("false");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.BOOL, token.getTokenType());
+        Assert.assertEquals("false", token.getValue());
+    }
+
+    @Test
+    public void testBoolValueTrue() {
+        Lexer lexer = new Lexer("true");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.BOOL, token.getTokenType());
+        Assert.assertEquals("true", token.getValue());
+    }
+
+    @Test
+    public void testIntegerType() {
+        Lexer lexer = new Lexer("int");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.INTEGER, token.getTokenType());
+        Assert.assertEquals("int", token.getValue());
+    }
+
+    @Test
+    public void testIntegerValue() {
+        Lexer lexer = new Lexer("42");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.INTEGER, token.getTokenType());
+        Assert.assertEquals("42", token.getValue());
+    }
+
+    @Test
+    public void testStringType() {
+        Lexer lexer = new Lexer("string");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.STRING, token.getTokenType());
+        Assert.assertEquals("string", token.getValue());
+    }
+
+    @Test
+    public void testStringValue() {
+        Lexer lexer = new Lexer("\"hello\"");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.STRING, token.getTokenType());
+        Assert.assertEquals("hello", token.getValue());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testStringValueMissingClosingQuotationMark() {
+        Lexer lexer = new Lexer("\"hello");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.STRING, token.getTokenType());
+        Assert.assertEquals("hello", token.getValue());
+    }
+
+    @Test
+    public void testIf() {
+        Lexer lexer = new Lexer("if");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.IF, token.getTokenType());
+        Assert.assertEquals("if", token.getValue());
+    }
+
+    @Test
+    public void testElse() {
+        Lexer lexer = new Lexer("else");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.ELSE, token.getTokenType());
+        Assert.assertEquals("else", token.getValue());
+    }
+
+    @Test
+    public void testFor() {
+        Lexer lexer = new Lexer("for");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.FOR, token.getTokenType());
+        Assert.assertEquals("for", token.getValue());
+    }
+
+    @Test
+    public void testWhile() {
+        Lexer lexer = new Lexer("while");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.WHILE, token.getTokenType());
+        Assert.assertEquals("while", token.getValue());
+    }
+
+    @Test
+    public void testBinaryTokens() {
+        Lexer lexer = new Lexer("+-*/");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.PLUS, token.getTokenType());
+        Assert.assertEquals("+", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.MINUS, token.getTokenType());
+        Assert.assertEquals("-", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.MULTIPLY, token.getTokenType());
+        Assert.assertEquals("*", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.DIVIDE, token.getTokenType());
+        Assert.assertEquals("/", token.getValue());
+    }
+
+    @Test
+    public void testAssign() {
+        Lexer lexer = new Lexer("=");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.ASSIGN, token.getTokenType());
+        Assert.assertEquals("=", token.getValue());
+    }
+
+    @Test
+    public void testBooleanOperator() {
+        Lexer lexer = new Lexer("== != > < >= <=");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.EQUAL, token.getTokenType());
+        Assert.assertEquals("==", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.NOT_EQUAL, token.getTokenType());
+        Assert.assertEquals("!=", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.GREATER, token.getTokenType());
+        Assert.assertEquals(">", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.LESS, token.getTokenType());
+        Assert.assertEquals("<", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.GREATER_EQUAL, token.getTokenType());
+        Assert.assertEquals(">=", token.getValue());
+
+        token = lexer.nextToken();
+        Assert.assertEquals(TokenType.LESS_EQUAL, token.getTokenType());
+        Assert.assertEquals("<=", token.getValue());
+    }
+
+    @Test
+    public void testNew() {
+        Lexer lexer = new Lexer("new");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.NEW, token.getTokenType());
+        Assert.assertEquals("new", token.getValue());
+    }
+
+    @Test
+    public void testNone() {
+        Lexer lexer = new Lexer(" ");
+
+        Token token = lexer.nextToken();
+        Assert.assertEquals(TokenType.NONE, token.getTokenType());
+        Assert.assertEquals("NONE", token.getValue());
     }
 
     @Test(expected = IllegalStateException.class)
     public void testPeakEndOfFile() {
-        Lexer lexer = new Lexer("/");
+        Lexer lexer = new Lexer("#");
 
         lexer.nextToken();
     }
 
     @Test(expected = IllegalStateException.class)
     public void testCommentMissingSecondSlash() {
-        Lexer lexer = new Lexer("// /");
+        Lexer lexer = new Lexer("## #");
 
         lexer.nextToken();
     }
 
     @Test(expected = IllegalStateException.class)
     public void testCommentMissingClosingSlashes() {
-        Lexer lexer = new Lexer("// ");
+        Lexer lexer = new Lexer("## ");
 
         lexer.nextToken();
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testReservedNotSupported() {
-        Lexer lexer = new Lexer("class");
+    @Test
+    public void testCommentCorrect() {
+        Lexer lexer = new Lexer("## hello ##");
 
         lexer.nextToken();
     }
