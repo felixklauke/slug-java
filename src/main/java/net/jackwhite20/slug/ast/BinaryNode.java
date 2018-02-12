cb544e1950ae9dd67a99aad699a1419cf8584309
JackWhite20 <phil.westf@web.de>
2018-02-13 00:48:06 +0100

    Add a few new AST nodes for basic operations

---
+++ src/main/java/net/jackwhite20/slug/ast/BinaryNode.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 "JackWhite20"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.jackwhite20.slug.ast;
+
+import net.jackwhite20.slug.lexer.Token;
+
+/**
+ * @author Philip 'JackWhite20' <silencephil@gmail.com>
+ */
+public class BinaryNode extends Node {
+
+    private Node left;
+
+    private Token operator;
+
+    private Node right;
+
+    public BinaryNode(Node left, Token operator, Node right) {
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Token getOperator() {
+        return operator;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+}
