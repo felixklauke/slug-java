44955ef7bc04004925d03e525ea33541c75d991f
JackWhite20 <phil.westf@web.de>
2018-02-13 00:58:19 +0100

    Add basic bootstrap logic

--- src/main/java/net/jackwhite20/slug/SlugBootstrap.java
+++ src/main/java/net/jackwhite20/slug/SlugBootstrap.java
@@ -16,12 +16,42 @@
 
 package net.jackwhite20.slug;
 
+import net.jackwhite20.slug.ast.Node;
+import net.jackwhite20.slug.lexer.Lexer;
+import net.jackwhite20.slug.parser.Parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
 /**
  * @author Philip 'JackWhite20' <silencephil@gmail.com>
  */
 public class SlugBootstrap {
 
     public static void main(String[] args) {
+        if (args.length == 1) {
+            File file = new File(args[0]);
+            if (file.exists()) {
+                try {
+                    byte[] bytes = Files.readAllBytes(file.toPath());
+                    String source = new String(bytes, Charset.forName("UTF-8"));
+                    Lexer lexer = new Lexer(source);
 
+                    Parser parser = new Parser(lexer);
+
+                    Node ast = parser.parse();
+
+                    // TODO: 13.02.2018 Interpreting
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.err.println("Slug source file '" + args[0] + "' does not exist");
+            }
+        } else {
+            System.err.println("Usage: java -jar slug.jar [Slug Source File]");
+        }
     }
 }
